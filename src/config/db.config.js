8f690f9f469d66e383a404a045e402929824a17c
Pranto Deb <rajprit0m@outlook.com>
2025-11-13 03:01:39 +0600

    [Project Structure and DB Connenct]

---
+++ src/config/db.config.js
@@ -0,0 +1,24 @@
+import { MongoClient, Db } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * @type {Db}
+ */
+
+let db;
+
+export const connectDB = async () => {
+  try {
+    const client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
+    db = client.db();
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ DB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+export const getDB = () => db;
